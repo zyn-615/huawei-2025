db34419b67b84fba19224d36559ff86bc08d26b0
deltax <3258185434@qq.com>
2025-03-16 20:51:38 +0800

    update

--- main.cpp
+++ main.cpp
@@ -558,8 +558,8 @@ void read_action(int time)
 
 int main()
 {
+    std::cerr << "start input global information" << std::endl;
     scanf("%d%d%d%d%d", &T, &M, &N, &V, &G);
-
     for (int i = 1; i <= M; i++) {
         for (int j = 1; j <= (T - 1) / FRE_PER_SLICING + 1; j++) {
             scanf("%*d"); 
@@ -577,7 +577,7 @@ int main()
             scanf("%*d");
         }
     }
-
+    std::cerr << "end input global information" << std::endl;
     init();
     printf("OK\n");
     fflush(stdout);
@@ -587,10 +587,20 @@ int main()
     }
 
     for (int t = 1; t <= T + EXTRA_TIME; t++) {
+        std::cerr << "start time " << t << std::endl;
+        std::cerr << "start timestamp_action" <<std::endl;
         timestamp_action();
+        std::cerr << "end timestamp_action" <<std::endl;
+        std::cerr << "start delete_action" <<std::endl;
         delete_action();
+        std::cerr << "end delete_action" <<std::endl;
+        std::cerr << "start write_action" <<std::endl;
         write_action();
+        std::cerr << "end write_action" <<std::endl;
+        std::cerr << "start read_action" <<std::endl;
         read_action(t);
+        std::cerr << "end read_action" <<std::endl;
+        std::cerr << "end time " << t << std::endl;
     }
 
     return 0;
